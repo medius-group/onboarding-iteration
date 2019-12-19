5f0e2e5145a8fa95f4f5d054d7edea817eda8544
freelife2010 <snowman190011@gmail.com>
2019-12-19 11:36:14 +0100

    update Welcome back page with CTA

--- src/containers/Main/WelcomeBack/WelcomeBack.js
+++ src/containers/Main/WelcomeBack/WelcomeBack.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
+import iStockImg from 'assets/img/iStock.svg';
 
 const WelcomeBackWrapper = styled.div`
+  .welcome-back-info {
+    display: flex;
+    align-items: center;
 
+    .info-description {
+      a {
+        color: var(--color-blue);
+      }
+
+      p:last-child {
+        margin-top: 20px;
+      }
+    }
+  }
+
+  .onboarding-status-wrapper {
+    h6 {
+      text-align: center;
+    }
+  }
 `;
 
 function WelcomeBack() {
   return (
     <WelcomeBackWrapper>
-      <h4>Welcome Back page</h4>
+      <h4>Welcome Back!</h4>
+      <div className="welcome-back-info">
+        <div className="info-description">
+          <p>
+            You are on the right track, keep on working through the process and
+            your MediusFlow integration will work smooth!
+          </p>
+          <p>
+            We will guide you through this process, if you have any questions
+            don’t hesitate to <a href="/#">contact us</a>, read our{' '}
+            <a href="/#">FAQ</a> or ask questions in{' '}
+            <a href="/#">our community.</a>
+          </p>
+        </div>
+        <img src={iStockImg} alt="stock" />
+      </div>
+      <div className="onboarding-status-wrapper">
+        <h6>Status of onboarding</h6>
+      </div>
     </WelcomeBackWrapper>
   );
 }
